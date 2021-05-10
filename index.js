6437e107df840f0f996b7825c33c162173cd1d26
Nusrat-Jahan <jahannusrat735@gmail.com>
2021-05-10 08:13:49 +0200

    Removed cors

--- index.js
+++ index.js
@@ -13,22 +13,22 @@ let allowedOrigins = [
   "http://localhost:8080",
   "http://localhost:1234"
 ];
-// app.use(cors());
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        // If a specific origin isn’t found on the list of allowed origins
-        let message =
-          "The CORS policy for this application doesn’t allow access from origin " +
-          origin;
-        return callback(new Error(message), false);
-      }
-      return callback(null, true);
-    }
-  })
-);
+app.use(cors());
+// app.use(
+//   cors({
+//     origin: (origin, callback) => {
+//       if (!origin) return callback(null, true);
+//       if (allowedOrigins.indexOf(origin) === -1) {
+//         // If a specific origin isn’t found on the list of allowed origins
+//         let message =
+//           "The CORS policy for this application doesn’t allow access from origin " +
+//           origin;
+//         return callback(new Error(message), false);
+//       }
+//       return callback(null, true);
+//     }
+//   })
+// );
 const { check, validationResult } = require("express-validator");
 morgan = require("morgan");
 const mongoose = require("mongoose");
